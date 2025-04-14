7948e05b5f739b7374943dfa351b3de2febdf3bc
K Keshav <126003131@sastra.ac.in>
2025-04-14 18:00:34 +0530

    added 4 new models

--- HateSpeechDetector/forms.py
+++ HateSpeechDetector/forms.py
@@ -3,6 +3,6 @@ from django import forms
 
 class CommentForm(forms.Form):
     content = forms.CharField(widget=forms.TextInput())
-    mode = forms.ChoiceField(choices=(('lstm2x', 'lstm2x'), ('biGRU', 'biGRU'),('distilBERT','distilBERT')))
+    mode = forms.ChoiceField(choices=(('lstm2x', 'lstm2x'), ('biGRU', 'biGRU'), ('ensemble', 'Ensemble'), ('rf', 'Random Forest'), ('gbc','Gradient Boosting'), ('xgb', 'XGBoost')))
 
 
--- HateSpeechDetector/views.py
+++ HateSpeechDetector/views.py
@@ -13,11 +13,19 @@ from keras._tf_keras.keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from django.http import JsonResponse
 from django.views.decorators.http import require_GET
+from nltk.stem import WordNetLemmatizer
 import json
 import os
+import joblib
 
 
-MAX_LEN = 294 #used for padding sequences in training
+nltk.download('wordnet')
+
+stop_words = set(stopwords.words('english'))
+lemmatizer = WordNetLemmatizer()
+
+
+MAX_LEN = 294  # used for padding sequences in training
 
 nltk.download('stopwords')
 stp = set(stopwords.words("english"))
@@ -28,8 +36,23 @@ with open("biGRU.pkl", 'rb') as file:
 with open("lstm2x.pkl", 'rb') as file:
     lstm2x = pickle.load(file)
 
+with open("rf.joblib", 'rb') as file:
+    rf = joblib.load(file)
+
+with open("ensemble_rgx.joblib", 'rb') as file:
+    ensemble = joblib.load(file)
+
+with open("gbc.joblib", 'rb') as file:
+    gbc = joblib.load(file)
+
+with open("xgb.joblib", 'rb') as file:
+    xgb = joblib.load(file)
+
 with open("tokenizer.pkl", 'rb') as f:
     lstm_tokenizer = pickle.load(f)
+
+with open("tf_idf.pkl", 'rb') as f:
+    vectorizer = pickle.load(f)
 
 
 def preprocess(text):
@@ -43,23 +66,82 @@ def preprocess(text):
     return padded
 
 
+def preprocess_1(text):
+    # Clean and tokenize
+    text = re.sub(r'[^a-zA-Z\s]', '', text.lower())
+    tokens = text.split()
+    tokens = [lemmatizer.lemmatize(w) for w in tokens if w not in stop_words]
+    clean_text = ' '.join(tokens)
+
+    # Vectorize
+    tfidf_vector = vectorizer.transform([clean_text])
+
+    # Add text length feature (same as training)
+    text_len = len(clean_text.split())
+
+    from scipy.sparse import hstack
+    final_input = hstack([tfidf_vector, [[text_len]]])  # sparse + dense combined
+
+    return final_input
+
+
 def detectHate(content, mode):
     # ml model pkl loaded will come here and model.predict will be called.
-    input = preprocess(content)
+    if mode not in ["lstm2x", "biGRU", "ensemble", "rf", "gbc", "xgb"]:
+        return JsonResponse({"error": "Invalid model name."}, status=400)
+
     if mode == 'lstm2x':
+        input = preprocess(content)
         prediction = lstm2x.predict(input)
         if prediction > 0.5:
             return True
         else:
             return False
+
     elif mode == 'biGRU':
+        input = preprocess(content)
         prediction = biGRU.predict(input)
         if prediction > 0.5:
             return True
         else:
             return False
-    elif mode == 'distilBERT':
-        return False
+
+    elif mode == 'ensemble':
+        print("here")
+        input = preprocess_1(content)
+        prediction = ensemble.predict(input)
+        print(prediction)
+        if prediction[0] == 1:
+            return True
+        else:
+            return False
+
+    elif mode == 'gbc':
+        input = preprocess_1(content)
+        prediction = gbc.predict(input)
+        print(prediction)
+        if prediction[0] == 1:
+            return True
+        else:
+            return False
+
+    elif mode == 'xgb':
+        input = preprocess_1(content)
+        prediction = xgb.predict(input)
+        print(prediction)
+        if prediction[0] == 1:
+            return True
+        else:
+            return False
+
+    elif mode == 'rf':
+        input = preprocess_1(content)
+        prediction = rf.predict(input)
+        print(prediction)
+        if prediction[0] == 1:
+            return True
+        else:
+            return False
 
 
 # Create your views here.
@@ -67,7 +149,7 @@ def detectHate(content, mode):
 def model_info_view(request):
     model = request.GET.get("model")
 
-    if model not in ["lstm2x", "biGRU", "model3"]:
+    if model not in ["lstm2x", "biGRU", "ensemble", "rf", "gbc", "xgb"]:
         return JsonResponse({"error": "Invalid model name."}, status=400)
 
     base_url = f"/static/reports/{model}"
@@ -87,6 +169,7 @@ def home(request):
         if form.is_valid():
             content = form.cleaned_data['content']
             mode = form.cleaned_data['mode']
+            print(mode)
             isHate = detectHate(content, mode)
             c = Comment(content=content, isHate=isHate)
             c.save()
